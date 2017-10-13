5bfcbe3778ead23ecadc1243186e778480e26bf5
Aaron Newton <ajn54@pitt.edu>
2017-10-12 20:22:03 -0400

    updated website files

---
+++ webFiles/style.js
@@ -0,0 +1,12 @@
+myID = document.getElementById("myID");
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 1) {
+        myID.className = "title show"
+    } else {
+        myID.className = "title hide"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
